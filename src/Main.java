74f906156321354954015ea97631b49685f2a80d
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-02 18:58:31 +0100

    Introducción - anterior y siguiente

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-/*El programa que que se proporciona en el fichero suma.py toma dos números enteros y muestra en pantalla su suma.
-Complete la función main de la clase Suma (fichero Suma.java) para que haga lo mismo.
+/*
+El programa que se proporciona escrito en Python lee un valor entero del teclado y muestra en pantalla los valores
+anterior y siguiente. Por ejemplo, si se introduce 5, se muestran en pantalla el 4 y el 6, en este orden.
+
+Complete el programa que se proporciona escrito en Java para que haga lo mismo.
  */
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Introduce first number: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Introduce second number: ");
-        int num2 = scanner.nextInt();
-        System.out.println(num1+num2);
+        System.out.print("Enter number: ");
+        int input = scanner.nextInt();
+        System.out.println(input+1);
+        System.out.println(input-1);
     }
 }
