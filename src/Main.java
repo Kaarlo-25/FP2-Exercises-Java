3c0fbc866bbbad4f29a4a8fa8a95cfbed9b529f2
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-02 18:50:00 +0100

    commit test

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/*El programa que que se proporciona en el fichero suma.py toma dos números enteros y muestra en pantalla su suma.
+Complete la función main de la clase Suma (fichero Suma.java) para que haga lo mismo.
+ */
+public class Main{
+    public static void main(String[] args){
+
+    }
+}
