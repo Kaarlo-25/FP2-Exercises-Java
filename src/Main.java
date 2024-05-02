6fd10d3c272929348bd627abfda35e20690d9136
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-02 21:50:16 +0100

    Strings y regex - palabras acabadas en "mente"

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,26 @@
 /*
-Añada a la clase Functions, que se proporciona escrita en Java, una función pública de clase llamada swap que admita
-como parámetro una string y devuelva una nueva string que contenga las mismas palabras que la pasada por parámetro,
-en orden inverso, separadas por un único espacio y con la incial de cada palabra en mayúscula y el resto en minúscula.
-La string pasada como parámetro solo contiene palabras y espacios.
+Añada a la clase ArrayTools un método público de clase llamado palabrasEnMente que reciba un array de Strings y
+devuelva un nuevo array de Strings. El array devuelto debe tener el mismo tamaño que el pasado como parámetro. El
+array pasado como parámetro contiene una lista de palabras. El array resultante contendrá las mismas palabras, excepto
+aquellas que terminen en "mente", que se modificarán eliminando ese sufijo.
+Por ejemplo:
+Entrada: {"El", "gato", "insolentemente", "saltaba", "ágilmente" }
+Salida: {"El", "gato", "insolente", "saltaba", "´´ágil" }
 */
-
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "Hola mundo de Diana";
-        System.out.println(swap(text));
+        String[] array = {"El", "gato", "insolentemente", "saltaba", "ágilmente"};
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(palabrasEnMente(array)));
     }
-    public static String swap(String text){
-        String[] textList = text.split("\\s");
-        String[] result = new String[textList.length];
-        for (int i=0; i<textList.length; i++){
-            String word = textList[i].toLowerCase();
-            String firstLetter = String.valueOf(word.charAt(0));
-            word = firstLetter.toUpperCase() + word.substring(1, word.length());
-            textList[i] = word;
+    public static String[] palabrasEnMente(String[] array){
+        for (int i=0; i<array.length; i++){
+            if (array[i].endsWith("mente")) {
+                array[i] = array[i].replaceAll("mente", "");
+            }
         }
-        int j = 0;
-        for (int i=textList.length-1; i >= 0; i--){
-            result[j] = textList[i];
-            j++;
-        }
-        return Arrays.toString(result);
+        return array;
     }
 }
