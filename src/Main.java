0122aca1c9cce7dc5e5165e53f6d1dee2683a906
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-02 20:57:34 +0100

    Arrays - array is sorted?

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,30 @@
 /*
-Añada a la clase ArrayTools un método público de clase llamado concatenar que acepte como parámetros un array de
-elementos de tipo String y un valor de tipo String. El método debe devolver una string formada por la concatenación
-de todos los elementos del array pasado como primer parámetro usando como separador la string pasada como segundo
-parámetro.
+Añada a la clase ArrayTools un método público de clase llamado isSorted que acepte como parámetro un array de elementos
+de tipo int y devuelva un resultado de tipo boolean.
 
-Nota: el operador + permite calcular la concatenación de dos strings.
+El resultado será true si los elementos del array están ordenados en orden creciente y false si no lo están.
+
+Un array está ordenado en orden creciente si cada elemento, menos el primero, es mayor o igual que el anterior. Un array
+vacío, o con un solo elemento, se considera ordenado.
  */
 
 
 public class Main{
     public static void main(String[] args){
-        String[] array = {"Hola", "mundo", "como", "estas?"};
-        String ch = " ";
-        System.out.println(concatenar(array, ch));
+        int[] array = {1,2,4,4,5,6,7};
+        int[] array1 = {3,1,4,5,6,7,8};
+        System.out.println(isSorted(array));
+        System.out.println(isSorted(array1));
     }
-    public static String concatenar(String[] array, String ch){
-        String result = "";
-        for (String word: array){
-            result += word + ch;
+    public static boolean isSorted(int[] array){
+        int num = array[0];
+        for (int i=1; i < array.length; i++){
+            if (array[i] < num){
+                return false;
+            } else{
+                num = array[i];
+            }
         }
-        return result;
+        return true;
     }
 }
