49fb407da3f9db2cc367b544e69022f59141133a
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-02 19:43:35 +0100

    Introducción - km a millas

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 /*
-Escriba un programa en Java que lea del teclado dos números enteros que representan la base y la altura de un
-rectángulo y muestre en pantalla su perímetro.
+La siguiente función en Python toma como parámetro un número real, que representa una distancia en kilómetros y
+devuelva su equivalente en millas.
+
+def km2mi(km: float):
+    """Convierte de kilómetros a millas"""
+    return km * 0.621371
+
+Tradúzcala a una función public static en Java, con el mismo nombre, que tome un parámetro de tipo double y devuelva
+un resultado de tipo double.
+
+Nota: 1 Km equivale a 0.621371 mi
  */
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Base: ");
-        int base = scanner.nextInt();
-        System.out.print("Altura: ");
-        int height = scanner.nextInt();
-        System.out.println(2*base + 2*height);
+        System.out.print("Km: ");
+        double input = scanner.nextDouble();
+        System.out.printf("Miles: %f", km2miles(input));
+    }
+    public static double km2miles(double km){
+        return km * 0.621371;
     }
 }
