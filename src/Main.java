fc00069d00741d99c12b864dccf2197bd2572f71
Kaarlo <kaarlocaballeronillukka@gmail.com>
2024-05-16 13:08:34 +0100

    Herencia - Point2D

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 /*
-Se proporciona la implementación en Python de la clase Rectángulo así como una plantilla para implementear la misma
-clase en Java. A partir de esta plantilla, complete la implementación de la clase Rectángulo en Java para que funcione
-como la de Python.
+Se proporciona la implementación en Python de la clase Point2D así como una plantilla para implementar la misma clase
+en Java. A partir de esta plantilla, complete la implementación de la clase Point2D en Java para que funcione como la
+de Python.
 
-Nota: en Java consideraremos que los lados de un rectángulo son valores de tipo double.
+Notas:
+En Java consideraremos que las coordenadas de un punto son valores de tipo double.
+El método coordinates devuelve en Python una tupla de dos números float. En Java debe devolver un array de dos números
+double.
 */
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(3, 5);
-        System.out.println(r);
+        Point2D p = new Point2D(2, 3);
+        System.out.println(p);
     }
 }
 
